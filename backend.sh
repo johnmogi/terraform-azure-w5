e05087a83ab2c0fc91446b17aa97ec104095dec3
johnmogi <anguru@gmail.com>
2022-06-17 00:42:34 +0300

    up

--- backend.sh
+++ backend.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-apt update
-apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-apt install docker-ce -y
-docker pull postgres:latest
+apt update -y
+apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+    mkdir -p /etc/apt/keyrings
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg 
+ echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null -y
+  apt update -y
+  apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+  docker pull postgres:latest -y
 docker run -d  --restart=unless-stopped --name measurements -p 5432:5432 -e 'POSTGRES_PASSWORD=p@ssw0rd42' postgres
-
