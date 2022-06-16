17c38f38a2afe013779f18a8481da2e2b57adc53
johnmogi <anguru@gmail.com>
2022-06-16 06:22:41 +0300

    1

--- frontend.sh
+++ frontend.sh
@@ -10,14 +10,32 @@ nvm install 14
 mkdir /var/www ; cd /var/www
 git clone https://github.com/johnmogi/bootcamp-app.git
 cd bootcamp-app/
-mv * .. ; rsync .. ; cd .. ; rm -rf bootcamp-app
 npm i
-# connect ssh + pm2
+echo """
+PORT=8080
+HOST=0.0.0.0
+NODE_ENV=development
+HOST_URL=http://localhost:8080
+COOKIE_ENCRYPT_PWD=superAwesomePasswordStringThatIsAtLeast32CharactersLong!
+
+# Okta configuration
+OKTA_ORG_URL="https://dev-96311573.okta.com"
+OKTA_CLIENT_ID="0oa57uv2e4ZbMvc8b5d7"
+OKTA_CLIENT_SECRET="-dS3ittft57jAzdyGrR1AmJhVD9qr2xapEJla7MF"
+
+# Postgres configuration
+PGHOST=34.90.138.223
+PGUSERNAME=postgres
+PGDATABASE=postgres
+PGPASSWORD=hxfTU0875GCi
+PGPORT=5432
+""" > .env
+npm run initdb
 sudo npm i -g pm2
 # pm2 start start.sh --name weight
 pm2 start npm -- start --name weight
 # sudo env PATH=$PATH:/usr/local/bin pm2 startup -u azureuser
 sudo env PATH=$PATH:/home/azureuser/.nvm/versions/node/v14.19.3/bin /usr/local/lib/node_modules/pm2/bin/pm2 startup systemd -u azureuser --hp /home/azureuser
 sudo systemctl start pm2-azureuser
-pm2 save
+pm2 savecd ..
 
